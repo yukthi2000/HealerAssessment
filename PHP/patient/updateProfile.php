43585df9cc734f124ac28eb70ea8ba87a1184419
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-09-20 02:50:11 +0530

    tried file saving

--- PHP/patient/updateProfile.php
+++ PHP/patient/updateProfile.php
@@ -18,50 +18,50 @@ try {
     $data = json_decode(file_get_contents("php://input"), true);
 
     if (!isset($data['Patient_ID'])) {
-        throw new Exception('User_ID is not provided in the request.'+$data);
+        throw new Exception('User_ID is not provided in the request.');
     }
 
-    $stmt = $conn->prepare("UPDATE patient SET PhoneNo = :PhoneNo, Address = :Address ,SpecialDisease= :SpecialDisease WHERE Patient_ID = :Patient_ID");
-    $stmt->bindValue(':Patient_ID', $data['Patient_ID']);
-    $stmt->bindValue(':PhoneNo', $data['PhoneNo']);
-    $stmt->bindValue(':Address', $data['Address']);
-    $stmt->bindValue(':SpecialDisease', $data['SpecialDisease']);
+    // $stmt = $conn->prepare("UPDATE patient SET PhoneNo = :PhoneNo, Address = :Address ,SpecialDisease= :SpecialDisease WHERE Patient_ID = :Patient_ID");
+    // $stmt->bindValue(':Patient_ID', $data['Patient_ID']);
+    // $stmt->bindValue(':PhoneNo', $data['PhoneNo']);
+    // $stmt->bindValue(':Address', $data['Address']);
+    // $stmt->bindValue(':SpecialDisease', $data['SpecialDisease']);
 
 
-    $stmt->execute();
-    $rowCount = $stmt->rowCount();
-    if ($rowCount > 0) {
-        echo json_encode(array('message' => 'Profile details updated successfully'));
-    } else {
-        echo 'Patient details not found';
-    }
+    // $stmt->execute();
+    // $rowCount = $stmt->rowCount();
+    // if ($rowCount > 0) {
+    //     echo json_encode(array('message' => 'Profile details updated successfully'));
+    // } else {
+    //     echo 'Patient details not found';
+    // }
 
-        if (isset($_FILES['Profile'])) {
-            echo "awoo";
-    } else {
-        echo 'No file uploaded';
-    }
+    //     if (isset($_FILES['Profile'])) {
+    //         echo "awoo";
+    // } else {
+    //     echo 'No file uploaded';
+    // }
     
 
-    // if (isset($_FILES['Profile']) ) {
-    //     $target_dir = "profile/users_pic/";
-    //     $target_file = $target_dir . basename($_FILES["Profile"]["name"]);
-    //     $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    if (isset($_FILES['Profile'])) {
+        $target_dir = "profile/users_pic/";
+        $target_file = $target_dir . basename($_FILES["Profile"]["name"]);
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-    //     if (move_uploaded_file($_FILES["Profile"]["tmp_name"], $target_file)) {
-    //         $imageLink =  $target_file; 
-    //         $stmt = $conn->prepare("UPDATE patient SET profile = :profile WHERE Patient_ID = :Patient_ID");
-    //         $stmt->bindValue(':Patient_ID', $data['Patient_ID']);
-    //         $stmt->bindValue(':profile', $imageLink);
-    //         $stmt->execute();
-    //         echo json_encode(array('message' => 'Profile picture uploaded successfully'));
-    //     } else {
-    //         echo json_encode(array('error' => 'Sorry, there was an error uploading your file.'));
-    //     }
-    // }
-    // else{
-    //     echo json_encode(array('error' => 'No file uploaded'));
-    // }
+        if (move_uploaded_file($_FILES["Profile"]["tmp_name"], $target_file)) {
+            // $imageLink =  $target_file; 
+            // $stmt = $conn->prepare("UPDATE patient SET profile = :profile WHERE Patient_ID = :Patient_ID");
+            // $stmt->bindValue(':Patient_ID', $data['Patient_ID']);
+            // $stmt->bindValue(':profile', $imageLink);
+            // $stmt->execute();
+            echo json_encode(array('message' => 'Profile picture uploaded successfully'));
+        } else {
+            echo json_encode(array('error' => 'Sorry, there was an error uploading your file.', 'file' => $_FILES["Profile"]["name"]));
+        }
+    }
+    else{
+        echo json_encode(array('error' => 'No file uploaded'));
+    }
 
 
     
