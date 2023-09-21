c1323102e3f5fbed4c1f308799e5e4f16157700a
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-09-21 21:15:55 +0530

    added validation for medical request

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -181,6 +181,14 @@ const Profile = () => {
   const handleAddMedicalRequest = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
+    console.log(formData.get("duration"), formData.get("message"));
+    if (formData.get("consultationDate") === "" || formData.get("duration") === "" || formData.get("message") === "") {
+      toast.error("Please fill all the fields");
+      return;
+    }else if(formData.get("duration") < 0){
+      toast.error("Please enter a valid duration");
+      return;
+    }
     formData.append("patientID", sessionStorage.getItem("patientID"));
     const data = Object.fromEntries(formData);
     console.log(data);
@@ -190,7 +198,11 @@ const Profile = () => {
         console.log(err);
       });
     console.log(response);
-    alert(response.data.message);
+    if (response.data.message) {
+      toast.success(response.data.message);
+    } else {
+      toast.error(response.data.error);
+    }
   };
 
   const logout = () => {
@@ -380,7 +392,7 @@ const Profile = () => {
                   <br />
                   <div className="form-floating">
                     <input
-                      type="text"
+                      type="number"
                       className="form-control"
                       id="floatingPassword"
                       placeholder="New Password"
